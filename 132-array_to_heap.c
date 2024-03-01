65c124776a60e29ec9a02881374ecfb2ee29c0fb
Saviour Assandoh <asandohs@gmail.com>
2024-03-01 07:37:37 -0800

    task 41

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -66,7 +66,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
-#include "binary_trees.h"
 
 /**
  * array_to_heap - builds a Max Binary Heap tree from an array
